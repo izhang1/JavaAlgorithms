a92de28bc390519e2885dbcab9888309afce6c58
izhang1 <ivanzhangemail@gmail.com>
2015-07-11 12:31:45 -0400

    Added Java Files

---
+++ JavaAlgorithms/src/InsertionSort.java
@@ -0,0 +1,17 @@
+/* Author : Ivan Zhang
+ * 
+ * Insertion sorting algorithm. 
+ * - Stable
+ * - O(1) extra space
+ * - O(n^2) comparisons and swaps
+ * - Adaptive : O(n) time whne nearly sorted
+ * - Very low overhead
+ * 
+ * 
+ */
+
+public class InsertionSort {
+	public static void main(String [] args){
+		
+	}
+}
