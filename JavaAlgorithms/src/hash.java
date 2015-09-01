260a8235cbba45537554fa460f514ba6ab528a48
izhang1 <ivanzhangemail@gmail.com>
2015-09-01 17:09:13 -0400

    Added the add, and count function for trees

--- JavaAlgorithms/src/hash.java
+++ JavaAlgorithms/src/hash.java
@@ -1,24 +1,68 @@
 
 public class hash {
-	int hValue = 8191;
-	int arr[];
+	int hValue = 31;
+	String arr[][];
+	
 	public hash(){
-		arr = new int[100];
+		arr = new String[10000][100];
 	}
 	
 	//TODO: return a integer to use as the placement 
-	public int hashFunt(int num){
-		return (num % 8191);
+	private int hashFunt(String var){
+		int varValue = 0; 
+		for(int i = 0; i < var.length(); i++){
+			 varValue += var.charAt(i);
+		}
+		
+		return (varValue * hValue);
 	}
 	
 	//TODO: return an array with the same values of arr but doubled
-	public int[] doubArr(int arr[]){
+	private String[][] doubArr(String arr[][]){
 		return arr;
 	}
 	
-	//TODO: Add add the number to the specific position in the hash
-	public void add(int num){
+	private String[][] doubleInnerArr(String arr[][], int innerArrIndex){
+		return arr;
+	}
+	
+	//TODO: Add add the String to the specific position in the hash
+	private void add(String var){
+		int outArrIndex= hashFunt(var);
+		while(outArrIndex > arr.length) doubArr(arr);
 		
+		int innerArrLength = this.arr[outArrIndex].length;
+		if(!this.arr[outArrIndex][innerArrLength].isEmpty()) doubleInnerArr(this.arr, outArrIndex);
+		
+		int tempCount = 0;
+		while(tempCount < this.arr[outArrIndex].length ){
+			if(this.arr[outArrIndex][tempCount] == null){
+				this.arr[outArrIndex][tempCount] = var;
+				break;
+			}else{
+				tempCount++;
+			}
+		}
+	}
+	
+	private int getCount(String var){
+		int outArrIndex = hashFunt(var);
+		int varCount = 0;
+		int tempCount = 0;
+		while(tempCount < this.arr[outArrIndex].length ){
+			if(this.arr[outArrIndex][tempCount].equals(var)){
+				varCount++;
+			}
+		}
+		
+		return varCount;
+	}
+	
+	public static void main(String[] args){
+		hash obj = new hash();
+		obj.add("Hello");
+		obj.add("Hello");
+		obj.add("Hello");
 	}
 	
 	
