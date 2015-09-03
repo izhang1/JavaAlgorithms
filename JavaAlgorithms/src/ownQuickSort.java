bd470d9d7ca8f7560da52429c0108f3202446089
izhang1 <ivanzhangemail@gmail.com>
2015-09-03 14:16:22 -0400

    Added quicksort file and introduction to it

--- JavaAlgorithms/src/ownQuickSort.java
+++ JavaAlgorithms/src/ownQuickSort.java
@@ -1,4 +1,22 @@
+// Author : Ivan Zhang
+/*
+ * Why?
+ * Quicksort is one of the most applicable sorting algorithms out there. It runs at worst cast O(n^2) 
+ * however it averages O(nlogn) which is why this is a suitable algorithm. Furthermore it only uses 
+ * space complexity of O(1). 
+ * 
+ * When designing an algorithm it shouldn't be too complex. Quicksort is a few more lines than
+ * bubble sort, therefore making it applicable to code and debug as well.
+ * 
+ */
+
+import java.util.Arrays;
 
 public class ownQuickSort {
+	
+	public static void main(String args[]){
+		int array[] = {4,3,10,22,32,1,32,9,0,92};
+		//System.out.println(Arrays.toString(array));
 
+	}
 }
