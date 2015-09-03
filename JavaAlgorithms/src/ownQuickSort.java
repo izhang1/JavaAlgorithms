33a68b1d75ec8d9f146d23b109f9f4e54914141f
izhang1 <ivanzhangemail@gmail.com>
2015-09-03 14:47:11 -0400

    Added partition and quicksort code

--- JavaAlgorithms/src/ownQuickSort.java
+++ JavaAlgorithms/src/ownQuickSort.java
@@ -14,9 +14,37 @@ import java.util.Arrays;
 
 public class ownQuickSort {
 	
+	public int partition(int arr[], int left, int right){
+		int start = left;
+		int end = right;
+		int pivot = arr[(left + right) / 2];
+		while(start < end){
+			while(arr[start] < pivot) start++;
+			while(arr[end] > pivot) end--;
+			if(start <= end){
+				int temp = arr[start];
+				arr[start] = arr[end];
+				arr[end] = temp;
+				start++;
+				end--;
+			}
+		}
+		
+		return start;
+	}
+	
+	public void quickSort(int arr[], int left, int right){
+		int index = partition(arr, left,right);
+		if(left < index - 1) quickSort(arr, left, index - 1);
+		if(index < right) quickSort(arr, index, right);
+ 	}
+	
+
 	public static void main(String args[]){
 		int array[] = {4,3,10,22,32,1,32,9,0,92};
 		//System.out.println(Arrays.toString(array));
-
+		ownQuickSort obj = new ownQuickSort();
+		obj.quickSort(array, 0, array.length - 1);
+		System.out.println(Arrays.toString(array));
 	}
 }
